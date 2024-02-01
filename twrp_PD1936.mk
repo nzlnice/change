2199ae7ccd87bd439ec0345501f27004c7ad116e
nzlnice <3193782263@qq.com>
2024-02-01 20:39:08 +0800

    FIX CHANGE

---
+++ twrp_PD1936.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := PD1936
+PRODUCT_NAME := twrp_PD1936
+PRODUCT_BRAND := vivo
+PRODUCT_MODEL := V1936A
+PRODUCT_MANUFACTURER := vivo
